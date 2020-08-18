ea63e518006d26f7446f164286862a11ba02a1a7
Corey Bruyere <coreybruyere@ymail.com>
2020-08-18 12:33:49 -0700

    Add box story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,8 @@
 module.exports = {
-  "stories": [
+  stories: [
+    "../packages/**/src/components/**/*.stories.mdx",
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
