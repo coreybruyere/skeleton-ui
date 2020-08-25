d61c5a9861cc818d57861a2c5882e1d968c8d6d1
Corey Bruyere <coreybruyere@ymail.com>
2020-08-25 09:31:57 -0700

    Move introduction to docs and update Box docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: [
     "../packages/**/src/components/**/*.stories.mdx",
+    "../packages/**/src/docs/*.stories.mdx",
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
