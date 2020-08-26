954136e944506dc56bee6bf0e7017afc137ebee8
Corey Bruyere <coreybruyere@ymail.com>
2020-08-26 09:35:43 -0700

    WIP: Testing theme UI

--- packages/core/src/components/Box/Box.tsx
+++ packages/core/src/components/Box/Box.tsx
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
+import { InterpolationWithTheme } from '@emotion/core';
+import { css, get } from '@theme-ui/css';
 import {
   compose,
   space,
@@ -14,20 +16,49 @@ import {
   BorderProps,
   PositionProps,
 } from 'styled-system';
+import { createShouldForwardProp } from '@styled-system/should-forward-prop';
+
+// const shouldForwardProp = createShouldForwardProp([
+//   ...space.propNames,
+//   ...color.propNames,
+// ]);
+
+const sx = (props: any) => css(props.sx)(props.theme);
+const base = (props: any) => css(props.__css)(props.theme);
+const variant = ({ theme, variant, __themeKey = 'variants' }: any) =>
+  css(get(theme, __themeKey + '.' + variant, get(theme, variant)));
 
 export type BoxProps = SpaceProps &
   ColorProps &
   LayoutProps &
   FlexboxProps &
   BorderProps &
-  PositionProps;
+  PositionProps & {
+    as?: React.ElementType;
+    variant?: string;
+    css?: InterpolationWithTheme<any>;
+  };
+
+// export const Box = styled.div<BoxProps>(
+//   {
+//     boxSizing: 'border-box',
+//     minWidth: 0,
+//   },
+//   compose(space, color, layout, flexbox, border, position)
+// );
 
 export const Box = styled.div<BoxProps>(
   {
     boxSizing: 'border-box',
+    margin: 0,
     minWidth: 0,
   },
-  compose(space, color, layout, flexbox, border, position)
+  base,
+  variant,
+  space,
+  color,
+  sx,
+  props => props.css
 );
 
 // Implement theme-ui : https://github.com/system-ui/theme-ui/blob/master/packages/components/src/Box.js
