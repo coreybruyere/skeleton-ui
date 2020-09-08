ac66145e51025db42e85461ff697e262c1eba3c5
Corey Bruyere <coreybruyere@ymail.com>
2020-09-08 10:52:50 -0700

    Taxed from kodiak ui

--- packages/core/src/components/Box/Box.tsx
+++ packages/core/src/components/Box/Box.tsx
@@ -64,33 +64,198 @@
 
 // Implement theme-ui : https://github.com/system-ui/theme-ui/blob/master/packages/components/src/Box.js
 
+// import styled from '@emotion/styled';
+// import { Box as BaseBox, BoxProps as SpaceColorProps } from 'theme-ui';
+// import {
+//   compose,
+//   layout,
+//   flexbox,
+//   border,
+//   position,
+//   LayoutProps,
+//   FlexboxProps,
+//   BorderProps,
+//   PositionProps,
+// } from 'styled-system';
+
+// export type BoxProps = SpaceColorProps &
+//   LayoutProps &
+//   FlexboxProps &
+//   BorderProps &
+//   PositionProps;
+
+// /**
+//  * Extend theme-ui Box with layout, flexbox, border, & position props
+//  */
+// export const Box = styled(BaseBox)<BoxProps>(
+//   {
+//     boxSizing: 'border-box',
+//     minWidth: 0,
+//   },
+//   compose(layout, flexbox, border, position)
+// );
+
 import styled from '@emotion/styled';
-import { Box as BaseBox, BoxProps as SpaceColorProps } from 'theme-ui';
+import { css, IntrinsicSxElements, Theme, SxStyleProp } from 'theme-ui';
+import { SerializedStyles } from '@emotion/serialize';
+import { createShouldForwardProp } from '@styled-system/should-forward-prop';
 import {
-  compose,
+  space,
+  SpaceProps,
+  color,
+  ColorProps,
+  typography,
+  TypographyProps,
   layout,
-  flexbox,
-  border,
-  position,
   LayoutProps,
+  flexbox,
   FlexboxProps,
+  grid,
+  GridProps,
+  background,
+  BackgroundProps,
+  border,
   BorderProps,
+  position,
   PositionProps,
+  shadow,
+  ShadowProps,
+  get,
 } from 'styled-system';
 
-export type BoxProps = SpaceColorProps &
-  LayoutProps &
-  FlexboxProps &
-  BorderProps &
-  PositionProps;
+/**
+ * propNames are typed as string[] | undefined. Undefined is not
+ * an iterator so we have to cast propNames to only a string[]
+ */
+export const shouldForwardProp = createShouldForwardProp([
+  ...(space.propNames as string[]),
+  ...(color.propNames as string[]),
+  ...(typography.propNames as string[]),
+  ...(layout.propNames as string[]),
+  ...(flexbox.propNames as string[]),
+  ...(grid.propNames as string[]),
+  ...(background.propNames as string[]),
+  ...(border.propNames as string[]),
+  ...(position.propNames as string[]),
+  ...(shadow.propNames as string[]),
+]);
+
+export const systemProps = [
+  space,
+  color,
+  typography,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
+  shadow,
+];
 
 /**
- * Extend theme-ui Box with layout, flexbox, border, & position props
+ * sx function to pass the sx prop and theme
+ * into Theme UI's css function with parses the values in the
+ * prop and serializing them with the theme values
+ *
+ * @param props any
  */
-export const Box = styled(BaseBox)<BoxProps>(
+export function sx(props: {
+  theme: Theme;
+  sx?: SxStyleProp;
+}): SerializedStyles {
+  return css(props.sx as any)(props.theme);
+}
+
+/**
+ * base function allow the __base property to set default values
+ * that are read from the theme but can still be overridden by theme variants
+ */
+
+export type BaseProps = {
+  __base?: SxStyleProp;
+};
+
+export function base(props: { theme: Theme } & BaseProps) {
+  return css(props.__base as any)(props.theme);
+}
+
+/**
+ * variant
+ *
+ * Returns a function that accept's the components
+ * props. The variant and theme props are passed into `css`
+ * to generate the Emotion css that will be applied to the
+ * component
+ *
+ * Variants are defined in the theme with a key and then variant.
+ *
+ * {
+ *   buttons: {
+ *     primary: {
+ *       bg: 'primary',
+ *       color: 'white',
+ *     }
+ *   }
+ * }
+ */
+export type VariantProps = {
+  variant?: string;
+  variantKey?: string;
+};
+
+export function variant({
+  variant,
+  theme,
+  variantKey,
+}: { theme: Theme } & VariantProps) {
+  return css(
+    get(
+      theme,
+      variantKey ? `${variantKey}.${variant}` : (variant as string | number),
+      get(theme, variantKey as string | number)
+    )
+  )(theme);
+}
+
+export type SystemProps = SpaceProps &
+  ColorProps &
+  TypographyProps &
+  LayoutProps &
+  FlexboxProps &
+  GridProps &
+  BackgroundProps &
+  BorderProps &
+  PositionProps &
+  ShadowProps;
+
+type IntrinicElements = Omit<IntrinsicSxElements['div'], 'color'>;
+
+export interface BaseProp {
+  __base?: SxStyleProp;
+}
+
+export type BoxProps = {
+  as?: React.ElementType;
+} & BaseProp &
+  VariantProps &
+  SystemProps &
+  IntrinicElements;
+
+/**
+ * Box primitive component which is the base component for
+ * all components in Kodiak
+ */
+export const Box = styled<'div', BoxProps>('div', {
+  shouldForwardProp,
+})(
   {
     boxSizing: 'border-box',
+    margin: 0,
     minWidth: 0,
   },
-  compose(layout, flexbox, border, position)
+  base,
+  variant,
+  ...systemProps,
+  sx
 );
--- packages/core/src/components/Button/Button.tsx
+++ packages/core/src/components/Button/Button.tsx
@@ -1,20 +1,179 @@
-import React from 'react';
-import { StyledComponent } from '@emotion/styled';
+// import React, { Ref } from 'react';
+// import { StyledComponent } from '@emotion/styled';
 
-import { BoxProps } from '../';
-import * as S from './styles';
+// import { BoxProps } from '../';
+// import * as S from './styles';
 
-export type StyledButtonProps = React.ComponentPropsWithoutRef<'button'> &
-  BoxProps & {
-    isLoading?: boolean;
-  };
+// export type StyledButtonProps = React.ComponentPropsWithoutRef<'button'> &
+//   BoxProps & {
+//     isLoading?: boolean;
+//   };
 
-export type ButtonProps = StyledComponent<'button', any, StyledButtonProps>;
+// export type ButtonProps = StyledComponent<'button', any, StyledButtonProps>;
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (props, ref) => (
-    <S.Button as="button" ref={ref} {...props}>
-      fdsa
-    </S.Button>
-  )
+// export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+//   (props, ref: Ref<HTMLButtonElement>) => (
+//     <S.Button as="button" ref={ref} {...props}>
+//       fdsa
+//     </S.Button>
+//   )
+// );
+
+// import React, { Ref, forwardRef } from 'react';
+// import styled, { StyledComponent } from '@emotion/styled';
+// import {
+//   border,
+//   compose,
+//   flexbox,
+//   layout,
+//   position,
+//   space,
+//   variant,
+//   BorderProps,
+//   FlexboxProps,
+//   LayoutProps,
+//   PositionProps,
+//   SpaceProps,
+// } from 'styled-system';
+// import { Box, BoxProps } from '../';
+
+// export type StyledButtonProps = BoxProps & {
+//   variant?: 'primary' | 'secondary' | 'transparent';
+// };
+
+// const defaultButtonStyles = {
+//   position: 'relative',
+//   fontFamily: 'body',
+//   fontSize: 2,
+//   fontWeight: 'regular',
+//   lineHeight: 'copy',
+//   borderRadius: 0,
+//   borderWidth: 2,
+//   borderStyle: 'solid',
+//   padding: 3,
+//   ml: 0,
+//   mr: 0,
+//   mb: 0,
+//   appearance: 'none',
+// };
+
+// const buttonVariants = variant({
+//   variants: {
+//     primary: {
+//       ...defaultButtonStyles,
+//       color: 'colors.text',
+
+//       '&:hover': {
+//         backgroundColor: 'colors.text',
+//       },
+//     },
+//     secondary: {
+//       ...defaultButtonStyles,
+//       color: 'colors.text',
+
+//       '&:hover': {
+//         backgroundColor: 'colors.text',
+//       },
+//     },
+//     transparent: {
+//       ...defaultButtonStyles,
+//       color: 'colors.text',
+
+//       '&:hover': {
+//         backgroundColor: 'colors.text',
+//       },
+//     },
+//   },
+// });
+
+// export type ButtonProps = React.DetailedHTMLProps<
+//   React.ButtonHTMLAttributes<HTMLButtonElement>,
+//   HTMLButtonElement
+// > &
+//   BoxProps;
+
+// const StyledButton = styled(Box)<ButtonProps>`
+//   ${buttonVariants}
+//   ${compose(space, layout, flexbox, border, position)}
+// `;
+
+// export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+//   ({ children, ...props }, ref: Ref<HTMLButtonElement>) => (
+//     <StyledButton ref={ref} py={2}>
+//       {children}
+//     </StyledButton>
+//   )
+// );
+
+// Button.defaultProps = {
+//   variant: 'primary',
+// };
+
+import { Theme } from 'theme-ui';
+import styled from '@emotion/styled';
+import {
+  Box,
+  BoxProps,
+  variant,
+  VariantProps,
+  systemProps,
+  shouldForwardProp,
+  sx,
+} from '../';
+
+// /**
+//  * base
+//  *
+//  * Generate the base CSS for the button component
+//  * that is aware of the Theme UI theme
+//  *
+//  * @param props
+//  */
+// export function base({ theme }: { theme: Theme }): SerializedStyles {
+//   return css({
+//     px: 3,
+//     py: 2,
+//     color: 'white',
+//     bg: 'primary',
+//     border: 0,
+//     borderRadius: 'default',
+//     '&:hover': {
+//       bg: 'secondary',
+//     },
+//   })(theme);
+// }
+
+export const buttonVariant = ({
+  variant: variantProp,
+  variantKey = 'buttons',
+  theme,
+}: { theme: Theme } & VariantProps) =>
+  variant({ variant: variantProp, theme, variantKey });
+
+export type ButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  VariantProps &
+  BoxProps;
+
+/**
+ * Button primitive component
+ */
+export const Button = styled(Box, {
+  shouldForwardProp,
+})<ButtonProps>(
+  {
+    appearance: 'none',
+    cursor: 'pointer',
+    display: 'inline-block',
+    fontSize: 'inherit',
+    lineHeight: 'inherit',
+    textAlign: 'center',
+    textDecoration: 'none',
+    transition: 'all 0.2s ease-in-out',
+  },
+  buttonVariant,
+  ...systemProps,
+  sx
 );
