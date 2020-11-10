769aa9182c545b26258bb182e618bd0087132867
Corey Bruyere <coreybruyere@ymail.com>
2020-11-10 08:39:16 -0800

    Add initial, unstyled Checkbox

---
+++ packages/core/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,16 @@
+import React, { forwardRef } from 'react';
+import { StyledComponent } from '@emotion/styled';
+
+import * as S from './styles';
+
+export type CheckboxProps = StyledComponent<
+  'checkbox',
+  any,
+  S.StyledCheckboxProps
+>;
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ children,  ...rest }, ref) => (
+    <S.Checkbox as={'input'} type="checkbox" ref={ref as any} {...rest} />
+  )
+);
