f554e9c56639a784c5fc0eb84a95d8de7b938f97
Corey Bruyere <coreybruyere@ymail.com>
2020-11-17 10:34:33 -0800

    Add basic toggle functionality

--- packages/core/src/components/Checkbox/Checkbox.tsx
+++ packages/core/src/components/Checkbox/Checkbox.tsx
@@ -11,71 +11,102 @@ export type StyledCheckboxProps = React.ComponentPropsWithoutRef<'input'> &
     error?: boolean;
   };
 
-const BaseIcon = styled(Base)`
+const checkboxIconStyles = (props: any) => css`
   color: 'red';
 
   &:hover {
-    color: ${(props: any) =>
-      props.error ? props.theme.colors.error : props.theme.colors.primary};
+    color: ${props.error
+      ? props.theme.colors.error
+      : props.theme.colors.primary};
   }
+
   input:focus ~ & {
-    color: ${(props: any) =>
-      props.error ? props.theme.colors.error : props.theme.colors.primary};
+    color: ${props.error
+      ? props.theme.colors.error
+      : props.theme.colors.primary};
   }
+
   input:checked:disabled ~ &,
   input:disabled ~ & {
-    color: ${(props: any) => props.theme.colors.muted};
+    color: ${props.theme.colors.muted};
   }
+
   input:checked ~ & {
-    color: ${(props: any) =>
-      props.error ? props.theme.colors.error : props.theme.colors.primary};
+    color: ${props.error
+      ? props.theme.colors.error
+      : props.theme.colors.primary};
   }
 `;
 
-const CheckedIcon = () => <BaseIcon>X</BaseIcon>;
+const CheckedIcon = (props: any) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
+    <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+  </svg>
+);
 
-const UncheckedIcon = () => <BaseIcon>[]</BaseIcon>;
+const UncheckedIcon = (props: any) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
+    <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" />
+  </svg>
+);
 
-const Unchecked = styled(UncheckedIcon)`
+const Checked = styled(CheckedIcon)`
   display: none;
+
   input:checked ~ & {
     display: block;
   }
+
+  ${checkboxIconStyles}
 `;
 
-const Checked = styled(CheckedIcon)`
+const Unchecked = styled(UncheckedIcon)`
   display: block;
+
   input:checked ~ & {
     display: none;
   }
+
+  ${checkboxIconStyles}
 `;
 
 const CheckboxIcon = (props: any) => (
   <>
-    <Unchecked {...props} />
     <Checked {...props} />
+
+    <Unchecked {...props} />
   </>
 );
 
 export const StyledCheckbox = styled(Base, {
   shouldForwardProp,
-})<StyledCheckboxProps>`
-  color: red;
-`;
+})<StyledCheckboxProps>``;
 
 export type CheckboxProps = StyledCheckboxProps;
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ children, ...rest }, ref) => (
+  ({ children, disabled, error, ...rest }, ref) => (
     <Label>
       <Flex>
         <StyledCheckbox
           as={'input'}
           type="checkbox"
           ref={ref as any}
+          disabled={disabled}
           {...rest}
+          position="absolute"
+          opacity={0}
+          zIndex={-1}
+          width={1}
+          height={1}
+          overflow="hidden"
         />
-        <Box as={CheckboxIcon}></Box>
+        <Box
+          as={CheckboxIcon}
+          aria-hidden="true"
+          disabled={disabled}
+          error={error}
+        />
       </Flex>
     </Label>
   )
