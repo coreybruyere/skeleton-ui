fb26845d6ab30214418568a1c60f53a37b24b1e5
Corey Bruyere <coreybruyere@ymail.com>
2020-12-03 15:05:14 -0800

    Implement basic Select input

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -7,6 +7,7 @@ export * from './Heading';
 export * from './Input';
 export * from './Label';
 export * from './Link';
+export * from './Select';
 export * from './Text';
 
 export * from './Styled';
