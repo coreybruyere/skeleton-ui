92eba7fb0275f17793166eb1d5fcdd4c7a72c4c0
Corey Bruyere <coreybruyere@ymail.com>
2021-01-19 09:44:34 -0800

    Add beginning of Stack component

---
+++ packages/core/src/components/Stack/Stack.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import { Grid, GridProps } from '../Grid';
+
+export type StyledStackProps = GridProps;
+
+export const StyledStack = styled(Grid)<StyledStackProps>``;
+
+export type StackProps = StyledStackProps & {
+  space: any;
+};
+
+export const Stack: React.FC<StackProps> = ({ children, ...rest }) => (
+  <StyledStack gridGap={4} {...rest}>
+    {children}
+  </StyledStack>
+);
