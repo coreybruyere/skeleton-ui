16ed3ac406e4bfaa1d137ffbfb7309e492afc16d
Corey Bruyere <coreybruyere@ymail.com>
2021-03-11 09:46:49 -0800

    Paradigm shift - no more referencing theme from styled HOC. Use props + sx prop and styled HOC as last resort

--- packages/core/src/components/Divider/Divider.tsx
+++ packages/core/src/components/Divider/Divider.tsx
@@ -16,10 +16,15 @@ export type StyledDividerProps = React.ComponentPropsWithoutRef<'hr'> &
   BoxProps &
   DividerDirection;
 
+/**
+ * This is where error was coming from: https://github.com/system-ui/theme-ui/issues/158
+ * Theme has to be accessed, from within a styled-component as an HOC, using @theme-ui/css css utility. This is a complete paradigm shift, but always resort to props first, sx prop second, and css utility as a fallback for more complex css even though sx and props should be able to handle everything
+ */
+
 export const StyledDivider = styled(Box)<StyledDividerProps>`
   overflow: visible;
   border: none;
-  background-color: ${({ theme }) => theme.colors.border};
+
   ${({ direction }) =>
     direction === 'vertical' &&
     `
@@ -39,6 +44,7 @@ export const Divider = forwardRef<HTMLHRElement, DividerProps>(
       width={direction === 'vertical' ? '1px' : 'auto'}
       mx={direction === 'vertical' ? 2 : 0}
       my={direction === 'horizontal' ? 2 : 0}
+      bg={'border'}
       ref={ref as any}
       role="separator"
       aria-orientation={direction}
